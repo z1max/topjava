35f0e67e05e84964e52b437b81875b9ed502d8a4
z1max <forcazimaxqwe123>
2018-03-18 18:31:26 +0300

    HW3 update sql scripts for meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,11 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories) VALUES
+  ('2018-03-18 10:00:00', 'Breakfast', 500),
+  ('2018-03-18 14:00:00', 'Lunch', 1000),
+  ('2018-03-18 20:00:00', 'Dinner', 700),
+  ('2018-03-17 10:00:00', 'Breakfast', 300),
+  ('2018-03-17 14:00:00', 'Lunch', 800),
+  ('2018-03-17 20:00:00', 'Dinner', 900);
