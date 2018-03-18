07a742729376f6f01d0108ba99dcf81645081159
z1max <forcazimaxqwe123>
2018-03-18 21:45:26 +0300

    HW3 fix sql scripts

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
@@ -27,10 +27,10 @@ CREATE TABLE user_roles
 
 CREATE TABLE meals
 (
-  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  user_id INTEGER NOT NULL,
-  date_time TIMESTAMP DEFAULT now() NOT NULL,
-  description VARCHAR NOT NULL,
-  calories INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (id)
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id     INTEGER                 NOT NULL,
+  date_time   TIMESTAMP DEFAULT now() NOT NULL,
+  description VARCHAR                 NOT NULL,
+  calories    INTEGER                 NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
