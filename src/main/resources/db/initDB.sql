9388f1d0bfc044a17a26f64bc0beb6fb021a2820
z1max <forcazimaxqwe123>
2018-03-18 18:39:22 +0300

    HW3 update sql scripts for meals - add foreign key

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -28,7 +28,9 @@ CREATE TABLE user_roles
 CREATE TABLE meals
 (
   id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id INTEGER NOT NULL,
   date_time TIMESTAMP DEFAULT now() NOT NULL,
   description VARCHAR NOT NULL,
-  calories INTEGER NOT NULL
+  calories INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -10,10 +10,10 @@ INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
 
-INSERT INTO meals (date_time, description, calories) VALUES
-  ('2018-03-18 10:00:00', 'Breakfast', 500),
-  ('2018-03-18 14:00:00', 'Lunch', 1000),
-  ('2018-03-18 20:00:00', 'Dinner', 700),
-  ('2018-03-17 10:00:00', 'Breakfast', 300),
-  ('2018-03-17 14:00:00', 'Lunch', 800),
-  ('2018-03-17 20:00:00', 'Dinner', 900);
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+  (100000, '2018-03-18 10:00:00', 'Breakfast', 500),
+  (100000, '2018-03-18 14:00:00', 'Lunch', 1000),
+  (100000, '2018-03-18 20:00:00', 'Dinner', 700),
+  (100000, '2018-03-17 10:00:00', 'Breakfast', 300),
+  (100000, '2018-03-17 14:00:00', 'Lunch', 800),
+  (100000, '2018-03-17 20:00:00', 'Dinner', 900);
