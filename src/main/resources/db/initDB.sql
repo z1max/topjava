c82320ff2516eecb4e33342ad59854d4a9d557a7
z1max <forcazimaxqwe123>
2018-03-19 22:53:43 +0300

    HW3 optional create index on meals

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -34,3 +34,4 @@ CREATE TABLE meals
   calories    INTEGER                 NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+CREATE UNIQUE INDEX user_id_Date_time_idx ON meals(user_id, date_time);
