9830e08eceae4610cfaee69d5edb9441bff17f8c
z1max <forcazimaxqwe123>
2018-04-24 19:24:49 +0300

    HW7 add ResourcesControllerTest

---
+++ src/test/java/ru/javawebinar/topjava/web/resources/ResourcesControllerTest.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.web.resources;
+
+import org.junit.Test;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+public class ResourcesControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void resources() throws Exception {
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/css"));
+    }
+}
